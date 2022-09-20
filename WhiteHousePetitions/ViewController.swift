4e149195cab47c231f05055526e0cdffd00970c5
Paulo Filho <pcf.feat@gmail.com>
2022-09-20 15:03:14 -0400

    study: wrap appropriate UI changes into DispatchQueue.main (that's UI thread)

--- WhiteHousePetitions/ViewController.swift
+++ WhiteHousePetitions/ViewController.swift
@@ -34,9 +34,9 @@ class ViewController: UITableViewController {
                     return
                 }
             }
+            
+            self?.showError()
         }
-        
-        showError()
     }
     
     @objc func creditsTapped() {
@@ -71,9 +71,11 @@ class ViewController: UITableViewController {
     }
     
     func showError() {
-        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        DispatchQueue.main.async { [weak self] in
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self?.present(ac, animated: true)
+        }
     }
     
     func parse(json: Data) {
@@ -82,7 +84,10 @@ class ViewController: UITableViewController {
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
             filteredPetitions = jsonPetitions.results
-            tableView.reloadData()
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
         }
     }
 
